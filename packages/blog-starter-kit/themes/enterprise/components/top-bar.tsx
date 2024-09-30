6674d3595960b658b467e4ecd5c51f14baf750bc
sidPhoenix17 <siddharthnibjiya@gmail.com>
2024-09-30 13:57:40 -0400

    Update top-bar.tsx

--- packages/blog-starter-kit/themes/enterprise/components/top-bar.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/top-bar.tsx
@@ -4,7 +4,7 @@ function TopBar() {
 	return (
 		<Link href={'https://tally.so/r/w5vp8Z'} target="_blank">
 			<section className="flex items-center justify-center gap-2 bg-violet-500 px-1 py-2 text-center text-sm text-white">
-				<p>Join Doctor Droid's Live Demo on 8th October.</p>
+				<p>Watch AI Investigation by Doctor Droid on 8th October.</p>
 				<button className="rounded border bg-white p-1 text-violet-500 transition-all hover:bg-violet-500 hover:text-white">
 					Register
 				</button>
